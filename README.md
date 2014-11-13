f9d847a842a3dcfce9f9ff7209dd44273854e757
fundon <cfddream@gmail.com>
2014-11-13 14:01:00 +0800

    update

--- README.md
+++ README.md
@@ -3,9 +3,45 @@
 
 SyncReader creates a reader that its reads to synchronize with first reader, similar to the `io.MultiWriter `.
 
+
+## APIs
+
+### `New(readers ...io.Reader)`
+
+First reader is `source reader`, other readers read to synchronize whith first reader.
+
+
 ## Usage
 
+Reading a file in progress.
+
 ```go
+import "github.com/go-libs/syncreader"
+import "github.com/go-libs/progress"
+
+filename := "syncreader_test.go"
+f, err := os.Open(filename)
+defer f.Close()
+if err != nil {
+  log.Fatalln(err)
+}
+
+fs, err := os.Stat(filename)
+if err != nil {
+  log.Fatalln(err)
+}
+
+p := progress.NewReader()
+p.Total = fs.Size()
+p.Progress = func(c, t, e int64) {
+  log.Println(c, t, e)
+}
+b := new(bytes.Buffer)
+r := syncreader.New(f, p)
+_, err = b.ReadFrom(r)
+if err != nil {
+  log.Fatalln(err)
+}
 ```
 
 
--- syncreader_test.go
+++ syncreader_test.go
@@ -2,38 +2,13 @@ package syncreader
 
 import (
 	"bytes"
-	"io"
 	"log"
 	"os"
 	"testing"
 
 	"github.com/bmizerany/assert"
+	"github.com/go-libs/progress"
 )
-
-type ProgressReader struct {
-	Current, Total, Expected int64
-	Progress                 func(current, total, expected int64)
-	isFinished               bool
-}
-
-func (p *ProgressReader) Read(b []byte) (n int, err error) {
-	if p.isFinished {
-		return 0, io.EOF
-	}
-	n = len(b)
-	p.isFinished = p.calculate(int64(n))
-	p.Progress(p.Current, p.Total, p.Expected)
-	return
-}
-
-func (p *ProgressReader) calculate(i int64) bool {
-	p.Current += i
-	if p.Current > p.Total {
-		p.Current = p.Total
-	}
-	p.Expected = p.Total - p.Current
-	return p.Current == p.Total
-}
 
 func TestNew(t *testing.T) {
 	filename := "syncreader_test.go"
@@ -48,7 +23,7 @@ func TestNew(t *testing.T) {
 		log.Fatalln(err)
 	}
 
-	p := new(ProgressReader)
+	p := progress.NewReader()
 	p.Total = fs.Size()
 	p.Progress = func(c, t, e int64) {
 		log.Println(c, t, e)
